5ff1a366cd192ac8dbed18449b5f568763a6fecb
mst8222 <mst8222@gmail.com>
2023-09-19 19:45:50 +0800

    change the index's icons

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -40,59 +40,67 @@ const CONFIG = {
   // 用户技能图标
   GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+      title_1: 'Ace Attorney',
+      img_1: '/images/heo/Ace Attorney.webp',
       color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_2: 'Nintendo',
+      img_2: '/images/heo/Nintendo.webp',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      title_1: 'Civ6',
+      img_1: '/images/heo/Civ6.webp',
       color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      title_2: 'MMH3',
+      img_2: '/images/heo/MMH3.webp',
       color_2: '#4082c3'
     },
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'FF7re',
+      img_1: '/images/heo/FF7re.webp',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_2: 'DQ11',
+      img_2: '/images/heo/DQ11.webp',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+      title_1: 'Warhammer-Totalwar',
+      img_1: '/images/heo/Warhammer-Totalwar.webp',
       color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+      title_2: 'MMH7.5',
+      img_2: '/images/heo/MMH7.5.webp',
       color_2: '#8f55ba'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
+      title_1: 'Mario&Rabbits',
+      img_1: '/images/heo/Mario&Rabbits.webp',
       color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      title_2: 'Zelda',
+      img_2: '/images/heo/Zelda.webp',
       color_2: '#2c51db'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
+      title_1: 'sc',
+      img_1: '/images/heo/sc.webp',
       color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      title_2: 'war3',
+      img_2: '/images/heo/war3.webp',
       color_2: '#e9572b'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      title_1: 'Xbox',
+      img_1: '/images/heo/Xbox.webp',
+      color_1: '#f7cb4f',
+      title_2: 'PS',
+      img_2: '/images/heo/PS.webp',
+      color_2: '#e9572b'
+    },
+    {
+      title_1: 'Steam',
+      img_1: '/images/heo/Steam.webp',
       color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      title_2: 'Epic',
+      img_2: '/images/heo/Epic.webp',
       color_2: '#1f1f1f'
     }
   ],
