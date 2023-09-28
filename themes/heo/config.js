195cdffd0983c36c4736a53cc8dce7a8c100d8bc
mst8222 <mst8222@gmail.com>
2023-09-28 11:44:36 +0800

    改tag为策略战棋

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,7 +19,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '策略游戏', url: '/tag/策略游戏' },
+  HERO_CATEGORY_3: { title: '策略战棋', url: '/tag/策略战棋' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HERO_RECOMMEND_POST_TAG: '推荐',
